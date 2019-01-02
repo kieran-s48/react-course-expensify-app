fd04afb21aa523f0eb8d192ac22eb8dcf776b2c2
Kieran Cole <kieran@space48.com>
2019-01-02 15:16:31 +0000

    Firebase install

--- src/app.js
+++ src/app.js
@@ -9,10 +9,11 @@ import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/lib/css/_datepicker.css';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import './firebase/firebase';
 
 const store = configureStore();
 
-console.log(store.getState());
+// console.log(store.getState());
 
 const jsx = (
     <Provider store={store}>
